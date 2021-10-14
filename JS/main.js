a09b4f415b6292868bfda90b00550e76d0f41a3d
Slepov Danil <Slepov_103@mail.ru>
2021-10-14 13:11:52 +0300

    burger

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,33 @@
 let registrationBtn = document.querySelector('.account__name');
 let closeRegistration = document.querySelector('.close_clean_form');
+let navMobile = document.querySelector('.header__nav_menu');
+let closeNavMobile = document.querySelector('.mobile__close');
+let windowWidth = window.innerWidth;
+
+let navMobileOutside =  document.querySelector('.header__mobile');
+
+document.addEventListener('click', outsideEvtListener);
+
+function outsideEvtListener(evt) {
+   console.log(evt.target);
+   console.log(navMobileOutside);
+    if (navMobileOutside.contains(evt.target)) {
+      document.querySelector('.header__mobile').classList.remove("scroll");
+    }
+}
+
 registrationBtn.addEventListener('click', function(){
    document.querySelector('.clean_form_block').style.display='block';
 });
 
 closeRegistration.addEventListener('click', function(){
    document.querySelector('.clean_form_block').style.display='none';
+});
+
+navMobile.addEventListener('click', function(){
+   document.querySelector('.header__mobile').classList.add("scroll");
+});
+
+closeNavMobile.addEventListener('click', function(){
+   document.querySelector('.header__mobile').classList.remove("scroll");
 });
